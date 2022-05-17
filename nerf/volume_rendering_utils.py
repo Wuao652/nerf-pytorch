976b38e717bf2dccda69029583c152f778cccae6
dennis <wuaoliu52@gmail.com>
2022-05-16 20:02:42 -0400

    add comments on volume_rendering functions!

--- nerf/volume_rendering_utils.py
+++ nerf/volume_rendering_utils.py
@@ -10,10 +10,36 @@ def volume_render_radiance_field(
     radiance_field_noise_std=0.0,
     white_background=False,
 ):
+    """
+    transform the radiance field to semantically meaningful values.
+    get the color, depth, opacity and disparity
+    Args:
+        radiance_field: [num_rays, N_samples, 4] the prediction of the neural network. e.g.(1024, 64, 4)
+        depth_values: [num_rays, N_samples along ray]. range in (near, far). e.g.(1024, 64)
+        ray_directions: [num_rays, 3]. direction of each ray e.g.(1024, 3)
+        radiance_field_noise_std: float
+        white_background: bool. If true, assume a white background.
+
+    Returns:
+        rgb_map: [num_rays, 3]. estimated RGB color of a ray.
+        disp_map: [num_rays] estimated disparity of a ray. 1 / depth.
+        acc_map: [num_rays] estimated opacity.
+        weights: [num_rays, N_samples]
+        depth_map: [num_rays] estimated depth of a ray.
+    """
+    # TODO: just for testing the algorithm
+    #       delete after learning the rendering
+    # seed = 42
+    # import numpy as np
+    # np.random.seed(seed)
+    # torch.manual_seed(seed)
+
     # TESTED
     one_e_10 = torch.tensor(
         [1e10], dtype=ray_directions.dtype, device=ray_directions.device
     )
+    # compute the delta_i for each query points.
+    # the delta for the last points of each ray are assigned to 1e10.
     dists = torch.cat(
         (
             depth_values[..., 1:] - depth_values[..., :-1],
@@ -21,6 +47,8 @@ def volume_render_radiance_field(
         ),
         dim=-1,
     )
+    # [num_rays, N_samples]
+    # dists times the norm of the ray directions
     dists = dists * ray_directions[..., None, :].norm(p=2, dim=-1)
 
     rgb = torch.sigmoid(radiance_field[..., :3])
@@ -34,8 +62,9 @@ def volume_render_radiance_field(
             )
             * radiance_field_noise_std
         )
-        # noise = noise.to(radiance_field)
+
     sigma_a = torch.nn.functional.relu(radiance_field[..., 3] + noise)
+
     alpha = 1.0 - torch.exp(-sigma_a * dists)
     weights = alpha * cumprod_exclusive(1.0 - alpha + 1e-10)
 
